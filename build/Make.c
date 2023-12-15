bbec350d7a3d44229e91835bf4481845af8112ce
Eatheun <ethan.al469@gmail.com>
2023-12-16 08:24:21 +1100

    Added Build Folder Organisation

--- build/Make.c
+++ build/Make.c
@@ -11,7 +11,7 @@
 #define RESET "\e[0m"
 #define WHTXT "\e[38;5;255m"
 #define BLKBK "\e[48;5;0m"
-#define MACCASXT "\e[1m\e[48;2;255;255;0m\e[38;2;255;0;0m"
+#define MACCASTXT "\e[1m\e[48;2;255;255;0m\e[38;2;255;0;0m"
 
 int main(void) {
     char *args[32];
@@ -19,11 +19,12 @@ int main(void) {
     int argIndex = 1;
 
     struct dirent *curr;
-    DIR *cwd = opendir("./");
+    char dirPath[] = "../src/";
+    DIR *cwd = opendir(dirPath);
     char *fn;
     while (curr = readdir(cwd)) {
         fn = curr->d_name;
-        if (strcmp(fn, "Make.c") != 0 && strstr(fn, ".cxx") != NULL) {
+        if (strstr(fn, ".cxx") != NULL) {
             // Cool colours :o
             int cuR = 72 + 20 * argIndex;
             int cuG = 48 + 12 * argIndex;
@@ -35,7 +36,11 @@ int main(void) {
             for (int i = 0; i < WHITESPACE - strlen(fn); i++) putchar(' ');
             printf(RESET);
             putchar('\n');
-            args[argIndex] = strdup(fn);
+
+            // Here we actually append the src file
+            char relPath[64];
+            sprintf(relPath, "%s%s", dirPath, fn);
+            args[argIndex] = strdup(relPath);
             argIndex++;
         }
     }
@@ -45,12 +50,14 @@ int main(void) {
     argIndex++;
     args[argIndex] = NULL;
 
-    printf("\n%s%sRunning:\n%sgcc ", BLKBK, WHTXT, MACCASXT);
+    // Compile!
+    printf("\n%s%sRunning:\n%sgcc ", BLKBK, WHTXT, MACCASTXT);
     for (int i = 1; i < argIndex; i++) printf("%s ", args[i]);
     printf(RESET);
     putchar('\n');
     spawnvp(P_WAIT, GCC_PATH, args);
 
+    // We duped earlier so we gotta free
     for (int i = 1; i < argIndex - 2; i++) free(args[i]);
 
     closedir(cwd);
